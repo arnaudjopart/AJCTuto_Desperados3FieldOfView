bd1ce881c360007204a1fa7f84a55daa93e34b36
arnaudjopart <arnaudjopart@users.noreply.github.com>
2020-10-27 23:24:21 +0100

    Rendering FOV

---
+++ Assets/_Tutorial/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public float m_primaryFieldOfViewDistance = 10f;
+    public float m_secondaryFieldOfViewDistance = 30f;
+
+    public float m_totalViewAngleInDegree = 40f;
+
+    public FieldOfView m_primaryFieldOfView;
+    public FieldOfView m_secondaryFieldOfView;
+
+    public float m_totalRotationAngle = 40f;
+
+    public float m_tweenDuration = 5f;
+
+    private float m_currentRotationAngle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_currentRotationAngle = -m_totalRotationAngle * .5f;
+        DOTween.To(() => m_currentRotationAngle, x => m_currentRotationAngle = x, m_totalRotationAngle * .5f,
+                m_tweenDuration)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.InOutQuad);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_primaryFieldOfView.DrawFieldOfView(m_totalViewAngleInDegree,m_primaryFieldOfViewDistance, m_currentRotationAngle);
+        m_secondaryFieldOfView.DrawFieldOfView(m_totalViewAngleInDegree,m_secondaryFieldOfViewDistance,m_currentRotationAngle);
+    }
+}
---
+++ Assets/_Tutorial/Scripts/FieldOfView.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+
+    public int m_nbOfTriangles = 10;
+
+    private MeshFilter m_meshFilter;
+
+    private void Awake()
+    {
+        m_meshFilter = GetComponent<MeshFilter>();
+    }
+
+    public void DrawFieldOfView(float _totalAngleOfView, float _distance, float _currentRotationAngle)
+    {
+        m_nbOfTriangles = Mathf.Max(1, m_nbOfTriangles);
+        var endPoints = new Vector3[m_nbOfTriangles + 1];
+        var step = m_nbOfTriangles == 1 ? _totalAngleOfView : _totalAngleOfView / m_nbOfTriangles;
+
+        for (var i = 0; i < endPoints.Length; i++)
+        {
+            var endPointDirection =
+                Quaternion.Euler(0, (-_totalAngleOfView * .5f) + (i * step), 0)
+                *Quaternion.Euler(0,_currentRotationAngle,0)
+                * transform.forward;
+            endPoints[i] = endPointDirection * _distance;
+        }
+        
+        var mesh = new Mesh();
+
+        var nbOfVertices = m_nbOfTriangles + 2;
+        var vertices = new Vector3[nbOfVertices];
+        vertices[0]=Vector3.zero;
+        
+        var uvs = new Vector2[nbOfVertices];
+        uvs[0] = Vector2.zero;
+
+        for (var i = 1; i < vertices.Length; i++)
+        {
+            vertices[i] = endPoints[i - 1];
+            uvs[i] = new Vector2(1,0);
+        }
+
+        var triangles = new int[m_nbOfTriangles * 3];
+        for (var i = 0; i < triangles.Length; i += 3)
+        {
+            triangles[i] = 0;
+            triangles[i + 1] = i / 3 + 1;
+            triangles[i + 2] = i / 3 + 2;
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uvs;
+
+        m_meshFilter.mesh = mesh;
+
+    }
+}
