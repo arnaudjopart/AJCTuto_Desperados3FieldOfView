03486e10551738b250c50d4e1d9071fb3a598a81
arnaudjopart <arnaudjopart@users.noreply.github.com>
2020-11-09 21:32:30 +0100

    commit

--- Assets/_Tutorial/Scripts/FieldOfView.cs
+++ Assets/_Tutorial/Scripts/FieldOfView.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+
 public class FieldOfView : MonoBehaviour
 {
 
